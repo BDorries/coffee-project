033606ef07af99cade7c08fbfde3f9021ddf012e
Andrea Varnado <andrea.h.varnado@gmail.com>
2023-01-11 12:32:39 -0600

    styling modal and images

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function renderCard(coffee){
 
     let html= `<div class="card">`;
     html += `<h2 class="card-title">${coffee.name}</h2>`;
-    html += `<img class="image" src="img/testCoffee.jpeg" alt="Coffee Pic">`;
+    html += `<img class="image" src="${coffee.image}" alt="Coffee Pic">`;
     html += `<h3 class="roast-type">${coffee.roast}</h3>`;
     html += `<hr>`;
     html += `<p class="card-body">`;
@@ -109,26 +109,29 @@ function displayModal(){
     modal.classList.add('hide');
 
 }
+let imgLight = "img/light-roast.svg";
+let imgMedium = "img/medium-roast.svg";
+let imgDark = "img/dark-roast.svg";
 
 let light = "Light brown in color, this roast is generally preferred for milder coffee varieties.";
 let medium = "This roast is medium brown in color with a stronger flavor and a non-oily surface.";
 let dark = "This roast produces shiny black beans with an oily surface and a pronounced bitterness.";
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
-    {id: 1, name: 'Light City', roast: 'light',description:light},
-    {id: 2, name: 'Half City', roast: 'light',description:light},
-    {id: 3, name: 'Cinnamon', roast: 'light',description:light},
-    {id: 4, name: 'City', roast: 'medium',description:medium},
-    {id: 5, name: 'American', roast: 'medium',description:medium},
-    {id: 6, name: 'Breakfast', roast: 'medium',description:medium},
-    {id: 7, name: 'High', roast: 'dark', description:dark},
-    {id: 8, name: 'Continental', roast: 'dark',description:dark},
-    {id: 9, name: 'New Orleans', roast: 'dark',description:dark},
-    {id: 10, name: 'European', roast: 'dark',description:dark},
-    {id: 11, name: 'Espresso', roast: 'dark',description:dark},
-    {id: 12, name: 'Viennese', roast: 'dark',description:dark},
-    {id: 13, name: 'Italian', roast: 'dark',description:dark},
-    {id: 14, name: 'French', roast: 'dark',description:dark},
+    {id: 1, name: 'Light City', roast: 'light',description:light, image:imgLight},
+    {id: 2, name: 'Half City', roast: 'light',description:light, image:imgLight},
+    {id: 3, name: 'Cinnamon', roast: 'light',description:light, image:imgLight},
+    {id: 4, name: 'City', roast: 'medium',description:medium, image:imgMedium},
+    {id: 5, name: 'American', roast: 'medium',description:medium, image:imgMedium},
+    {id: 6, name: 'Breakfast', roast: 'medium',description:medium, image:imgMedium},
+    {id: 7, name: 'High', roast: 'dark', description:dark, image:imgDark},
+    {id: 8, name: 'Continental', roast: 'dark',description:dark, image:imgDark},
+    {id: 9, name: 'New Orleans', roast: 'dark',description:dark, image:imgDark},
+    {id: 10, name: 'European', roast: 'dark',description:dark, image:imgDark},
+    {id: 11, name: 'Espresso', roast: 'dark',description:dark, image:imgDark},
+    {id: 12, name: 'Viennese', roast: 'dark',description:dark, image:imgDark},
+    {id: 13, name: 'Italian', roast: 'dark',description:dark, image:imgDark},
+    {id: 14, name: 'French', roast: 'dark',description:dark, image:imgDark},
 ];
 
 let modalBtn = document.getElementById('modalBtn');
