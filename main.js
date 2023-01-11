dacccf84dfc0b83f740e522bbcea461e24a988f0
Andrea Varnado <andrea.h.varnado@gmail.com>
2023-01-11 10:04:03 -0600

    updated style created login button

--- main.js
+++ main.js
@@ -9,6 +9,9 @@
 //
 //     return html;
 // }
+// function signInMenu(){
+//     let html = ``
+// }
 
 function renderCard(coffee){
 
@@ -18,7 +21,7 @@ function renderCard(coffee){
     html += `<h3 class="roast-type">${coffee.roast}</h3>`;
     html += `<hr>`;
     html += `<p class="card-body">`;
-    html += `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab dicta dolorem earum facilis iure ut.`;
+    html += `${coffee.description}`;
     html += `</p>`;
     html += `</div>`;
     return html;
@@ -48,11 +51,11 @@ function updateCoffees(e) {
         if (selectedRoast !== "All..." && coffee.roast !== selectedRoast){
             return;
         }
-        if (coffee.name.includes(recorded) && coffee.roast === selectedRoast) {
+        if ((coffee.name.toLowerCase().includes(recorded) || coffee.name.includes(recorded)) && coffee.roast === selectedRoast) {
             filteredCoffees.push(coffee);
             return;
         }
-        if(selectedRoast === "All..." && coffee.name.includes(recorded)){
+        if(selectedRoast === "All..." && (coffee.name.toLowerCase().includes(recorded) || coffee.name.includes(recorded))){
             filteredCoffees.push(coffee);
             return;
         }
@@ -70,22 +73,41 @@ function dropDown(){
 let btn1 = document.querySelector("#btn1");
 btn1.addEventListener('click', dropDown);
 
+//login functionality
+let username = document.querySelector("#username");
+let password = document.querySelector("#password");
+let loginBtn = document.querySelector("#loginBtn");
+
+function logIn(username,password){
+    if(username === "admin" && password === "admin"){
+    //TODO: display add a coffee function
+    }
+}
+
+//add a coffee function
+function addCoffee(){
+
+}
+
+let light = "Light brown in color, this roast is generally preferred for milder coffee varieties.";
+let medium = "This roast is medium brown in color with a stronger flavor and a non-oily surface.";
+let dark = "This roast produces shiny black beans with an oily surface and a pronounced bitterness.";
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
+    {id: 1, name: 'Light City', roast: 'light',description:light},
+    {id: 2, name: 'Half City', roast: 'light',description:light},
+    {id: 3, name: 'Cinnamon', roast: 'light',description:light},
+    {id: 4, name: 'City', roast: 'medium',description:medium},
+    {id: 5, name: 'American', roast: 'medium',description:medium},
+    {id: 6, name: 'Breakfast', roast: 'medium',description:medium},
+    {id: 7, name: 'High', roast: 'dark', description:dark},
+    {id: 8, name: 'Continental', roast: 'dark',description:dark},
+    {id: 9, name: 'New Orleans', roast: 'dark',description:dark},
+    {id: 10, name: 'European', roast: 'dark',description:dark},
+    {id: 11, name: 'Espresso', roast: 'dark',description:dark},
+    {id: 12, name: 'Viennese', roast: 'dark',description:dark},
+    {id: 13, name: 'Italian', roast: 'dark',description:dark},
+    {id: 14, name: 'French', roast: 'dark',description:dark},
 ];
 
 let cardsDiv = document.querySelector('div.cards');
